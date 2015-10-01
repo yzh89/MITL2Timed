cd98b329d4ebdc0400a7df28299cc5f6aa9aa2bc
Yuchen Zhou <yzh89@umd.edu>
2015-10-01 14:10:46 -0400

    add looped map for until

--- src/timed.c
+++ src/timed.c
@@ -1994,6 +1994,81 @@ TAutomata *create_map(int nodeNum){
   return t;
 }
 
+TAutomata *create_map_loop(int nodeNum){
+  // nodeNum>=4
+  if (nodeNum <4){
+    nodeNum =4;
+  }
+  TAutomata *t = (TAutomata *) tl_emalloc(sizeof(TAutomata));
+  TState *s = (TState *)tl_emalloc(sizeof(TState)*nodeNum);
+  CGuard *cguard;
+  int *clockId;
+
+  //share same cguard
+  cguard = (CGuard *) malloc(sizeof(CGuard));
+  cguard->nType = PREDICATE;
+  cguard->cCstr = (CCstr *)(CCstr * )  malloc(sizeof(CCstr));
+  cguard->cCstr->cIdx = cCount;
+  cguard->cCstr->gType = LESSEQUAL;
+  cguard->cCstr->bndry = 1;
+  // void create_tstate(TState *s, char *tstateId, CGuard *inv, unsigned short *input, unsigned short inputNum, unsigned short output, unsigned short buchi, Node* p){
+  for (int i=0; i<nodeNum; i++){
+    s[i].tstateId = (char *)malloc(sizeof(char)*6);
+    sprintf(s[i].tstateId, "loc%i",i);
+
+    create_tstate(&s[i], s[i].tstateId, cguard, (unsigned short *) 0, 0, 0, 0, NULL);
+    s[i].sym = new_set(3);
+    clear_set(s[i].sym,3);
+    add_set(s[i].sym, t_get_sym_id("a"));
+    add_set(s[i].sym, t_get_sym_id("b"));
+  }
+  s[nodeNum-1].output= 1<<t_get_sym_id("a");
+  s[nodeNum-3].output= 1<<t_get_sym_id("b");
+  TTrans *tt = emalloc_ttrans(1,1);
+  TTrans *tmp = tt;
+
+  //share same cguard and clockId
+  cguard = (CGuard*) malloc(sizeof(CGuard));
+  cguard->nType = PREDICATE;
+  cguard->cCstr = (CCstr * ) malloc(sizeof(CCstr));
+  cguard->cCstr->cIdx = cCount;
+  cguard->cCstr->gType = GREATEREQUAL;
+  cguard->cCstr->bndry = 1;
+  clockId = (int *) malloc(sizeof(int)*1);
+  clockId[0] = cCount;
+
+  for (int i=0; i<nodeNum-1; i++){
+      tmp->nxt = emalloc_ttrans(1,1);
+      tmp = tmp->nxt;
+      // void create_ttrans(TTrans *t, CGuard *cguard, int *cIdxs, int clockNum, TState *from, TState *to)
+      create_ttrans(tmp, cguard, clockId, 1, &s[i],  &s[i+1]);
+
+      tmp->nxt = emalloc_ttrans(1,1);
+      tmp = tmp->nxt;
+      // void create_ttrans(TTrans *t, CGuard *cguard, int *cIdxs, int clockNum, TState *from, TState *to)
+      create_ttrans(tmp, cguard, clockId, 1, &s[i+1],  &s[i]);
+
+  }
+  tmp->nxt = emalloc_ttrans(1,1);
+  tmp = tmp->nxt;
+  // void create_ttrans(TTrans *t, CGuard *cguard, int *cIdxs, int clockNum, TState *from, TState *to)
+  create_ttrans(tmp, cguard, clockId, 1, &s[nodeNum-1],  &s[0]);
+
+  tmp->nxt = emalloc_ttrans(1,1);
+  tmp = tmp->nxt;
+  // void create_ttrans(TTrans *t, CGuard *cguard, int *cIdxs, int clockNum, TState *from, TState *to)
+  create_ttrans(tmp, cguard, clockId, 1, &s[0],  &s[nodeNum-1]);
+
+  cCount++;
+
+  t->tTrans = tt->nxt;
+  t->tStates = s;
+  t->stateNum = nodeNum;
+  t->tEvents = NULL;
+  t->eventNum = 0;
+  return t;
+}
+
 /********************************************************************\
 |*                Display of the Timed Automata                     *|
 \********************************************************************/
@@ -2257,7 +2332,7 @@ void mk_timed(Node *p) /* generates an timed automata for p */
 
   print_timed(tAutomata);
 
-  TAutomata* mapAutomata = create_map(4);
+  TAutomata* mapAutomata = create_map_loop(4);
 
   print_timed(mapAutomata);
 
